49f6799792cf1835141ce6cfc8c4b44be5998b9b
Luka Jeremic <ljeremic4019rn@raf.rs>
2024-01-26 22:16:35 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static final String filePath = "data/germany.json";
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        DataLoader dataLoader = new DataLoader();
+
+
+    }
+}
---
+++ src/main/java/WWSite.java
@@ -0,0 +1,56 @@
+public class WWSite {
+
+    private String name;
+    private double capacity;
+    private double latitude;
+    private double longitude;
+
+    public WWSite(String name, double capacity, double latitude, double longitude) {
+        this.name = name;
+        this.capacity = capacity;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "WWSite{" +
+                "name='" + name + '\'' +
+                ", capacity=" + capacity +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
